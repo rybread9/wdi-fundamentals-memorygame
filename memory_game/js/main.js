68b4224751538c7ed45cccdf08bc827446e0dba9
Rylen del Rio <rylendelrio@Rylens-MacBook-Pro.local>
2018-08-22 13:40:25 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,21 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+    alert("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+    alert("Sorry, try again.");
+  }
+}
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+// console.log("User flipped " + cardThree);
+// console.log("User flipped " + cardFour);
